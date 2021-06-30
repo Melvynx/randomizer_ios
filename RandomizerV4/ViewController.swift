5ab4e47a8083ebaef07445c1f7114ea689459c94
Melvynx <melvyn.malherbe@qoqa.com>
2021-06-30 16:01:10 +0200

    Initial Commit

---
+++ RandomizerV4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomizerV4
+//
+//  Created by Melvyn Malherbe on 30.06.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
